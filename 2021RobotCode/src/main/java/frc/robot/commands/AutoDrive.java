2f024f5f1e8cc17bfdcd8c44628bff74bf9d0b54
powersurge4hprogramming <58537304+powersurge4hprogramming@users.noreply.github.com>
2021-03-23 19:31:10 -0400

    redo y -- with lerp slowdown

--- 2021RobotCode/src/main/java/frc/robot/commands/AutoDrive.java
+++ 2021RobotCode/src/main/java/frc/robot/commands/AutoDrive.java
@@ -1,9 +1,11 @@
+  
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.data_structs.Utilities;
@@ -14,7 +16,10 @@ public class AutoDrive extends CommandBase {
   private DriveTrainSubsys m_driveTrain;
 
   private int stepNum;
-  private int direction;
+ // private boolean done;
+  private double timeBetween;
+
+  private Timer time = new Timer();
 
   public AutoDrive(DriveTrainSubsys driveTrain) {
     m_driveTrain = driveTrain;
@@ -26,6 +31,8 @@ public class AutoDrive extends CommandBase {
   public void initialize() {
     stepNum = 3;
     m_driveTrain.resetEncoders();
+ //   done = false;
+    timeBetween = 1.5;
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -41,13 +48,21 @@ public class AutoDrive extends CommandBase {
       case 4:
         break;
       case 3:
-        trackDistance(0, 0.4, 60);
+        trackDistance(0.0, 0.35, 0, 0, 60);
         break;
       case 2:
-        trackDistance(0, -0.4, 60);
+      if (time.get()>timeBetween) {
+       // done = false;
+        trackDistance(0.0, -0.35, 0, 0, 60);
+     //   SmartDashboard.putBoolean("Done?", done);
+      }
         break;
       case 1:
-        trackDistance(0, 0.4, 60);
+      if (time.get()>timeBetween) {
+    //    done = false;
+      //  SmartDashboard.putBoolean("Done?", done);
+        trackDistance(0.0, 0.35, 0.0, 0, 60);
+      }
         break;
       case 0:
         m_driveTrain.setDrive(0, 0, 0);
@@ -57,22 +72,30 @@ public class AutoDrive extends CommandBase {
     }
   }
 
-  private void trackDistance(double speedX, double speedY, int targetY) {
-    if (Math.abs(m_driveTrain.getEncoderDist(0)) < targetY) {
-      double diff = targetY - Math.abs(m_driveTrain.getEncoderDist(0));
+  private void trackDistance(double speedX, double speedY, double speedZ, int targetX, int targetY) {
+    double sensorDist = Math.abs((m_driveTrain.getEncoderDist(0)+m_driveTrain.getEncoderDist(1))/2);
+    double sensorDistX = ((Math.abs(m_driveTrain.getEncoderDist(0))+Math.abs(m_driveTrain.getEncoderDist(1)))/2);
+  //  if (((targetY != 0 && sensorDist < targetY) || targetY == 0) && sensorDistX < targetX) {
+      if ( sensorDist < targetY) {
+
+      double diff = targetY - sensorDist;
       int slowDown = 14;
       int speedUp = 14;
-      if (Math.abs(m_driveTrain.getEncoderDist(0))<speedUp) {
-        double speedYLerped =  (-1*Math.signum(speedY)) * Utilities.lerp(0.15, Math.abs(speedY), (Math.abs(m_driveTrain.getEncoderDist(0) / speedUp)));
-        m_driveTrain.setDrive(speedX, speedYLerped, 0);
+      if (sensorDist<speedUp) {
+        double speedYLerped =  (-1*Math.signum(speedY)) * Utilities.lerp(0.15, Math.abs(speedY), sensorDist / speedUp);
+        m_driveTrain.setDrive(speedX, speedYLerped, speedZ);
       }
-      else if ((targetY-Math.abs(m_driveTrain.getEncoderDist(0)))<slowDown) {
-        double speedYLerped = (-1*Math.signum(speedY)) * Utilities.lerp(0.0, Math.abs(speedY), diff / slowDown);
-        m_driveTrain.setDrive(speedX,speedYLerped, 0);
+      else if ((targetY-sensorDist)<slowDown) {
+        double speedYLerped = (-1*Math.signum(speedY)) * Utilities.lerp((-1*Math.signum(speedY)*0.2), Math.abs(speedY), diff / slowDown);
+        m_driveTrain.setDrive(speedX, speedYLerped, speedZ);
       } else {
-        m_driveTrain.setDrive(speedX, -1 * speedY, 0);
+        m_driveTrain.setDrive(speedX, -1 * speedY, speedZ);
       }
     } else {
+      time.reset();
+      time.start();
+   //   done = true;
+     // SmartDashboard.putBoolean("Done?", done);
       m_driveTrain.setDrive(0, 0, 0);
       stepNum--;
       m_driveTrain.resetEncoders();
